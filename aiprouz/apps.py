2063da5cd80b0f2772a36f8b30de8ae4c1546cff
Timur <aitemintellex@gmail.com>
2024-04-26 16:16:46 +0500

    Hello, world!

---
+++ aiprouz/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AiprouzConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aiprouz'
---
+++ aiprouz/moon.py
@@ -0,0 +1,4 @@
+# aitemintellex/aiprouz/moonconfig.py
+import os
+# Constant - Указываю, что base_dir в папке aiprouz
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
