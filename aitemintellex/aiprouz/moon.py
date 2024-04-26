679b6eb7564e83af33313428203d943023771fe4
Timur Marufhodjaev <166158524+AitemIntellex@users.noreply.github.com>
2024-04-26 16:00:35 +0500

    Delete aitemintellex directory
    Загрузка была из неправильного места.

--- aitemintellex/aiprouz/moon.py
+++
@@ -1,4 +0,0 @@
-# aitemintellex/aiprouz/moonconfig.py
-import os
-# Constant - Указываю, что base_dir в папке aiprouz
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- aitemintellex/aitemintellex/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from aiprouz.views import home  # Импортируем функцию view, созданную ранее
-urlpatterns = [
-    path('', home, name='home'),  # Домашняя страница
-    path('admin/', admin.site.urls),
-]
